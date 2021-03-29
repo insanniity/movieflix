0f4da0f8b7d13576baa1dd78023981fa9eecec44
insanniity <cleydson.cg@live.com>
2021-03-29 13:52:24 -0300

    React Hook form basic integration

--- front-web/src/pages/Login/index.tsx
+++ front-web/src/pages/Login/index.tsx
@@ -1,8 +1,19 @@
 import './styles.scss';
 import {ReactComponent as LoginImg} from '../../core/assets/images/login.svg'
 import {ReactComponent as ArrowIcon } from '../../core/assets/images/arrow.svg';
+import { useForm } from "react-hook-form";
+
+type FormData ={
+    email:string;
+    password: string;
+}
 
 const Login = () => {
+    const { register, handleSubmit } = useForm<FormData>();
+    const onSubmit = (data : FormData) => {
+        console.log(data);
+    };
+
     return (
         <div className="row">
             <div className="offset-lg-2 offset-sm-1 col-lg-4 col-sm-12">
@@ -14,12 +25,23 @@ const Login = () => {
                 <div className="card card-login bg-secondary bd-radius-20">
                     <div className="card-body">
                         <h1 className="card-title login-form-title text-center">LOGIN</h1>
-                        <form>
+                        <form onSubmit={handleSubmit(onSubmit)}>
                             <div className="form-group">                                
-                                <input placeholder="Email" type="email" className="form-control" name="username" />                                
+                                <input 
+                                    placeholder="Email"
+                                    type="email"
+                                    className="form-control"
+                                    name="username"
+                                    ref={register({
+                                        required: "Campo obrigatório",
+                                        pattern: {
+                                            value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                                            message: "Email inválido"
+                                        }})}
+                                    />                                
                             </div>
                             <div className="form-group">                                
-                                <input placeholder="Senha" type="password" className="form-control" name="password"/>
+                                <input placeholder="Senha" type="password" className="form-control" name="password" ref={register({required: "Campo obrigatório"})}/>
                             </div> 
                             <div className="col-lg-12 text-center">
                                 <div className="default-btn">
@@ -35,8 +57,7 @@ const Login = () => {
                     </div>
                 </div>
             </div>
-        </div>
-        
+        </div>        
     )
 }
 
