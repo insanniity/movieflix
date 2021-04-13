bf68f8799b8db16d13cf179a87b7d5c87413ab50
insanniity <cleydson.cg@live.com>
2021-04-13 11:46:38 -0300

    Update index.ts

--- front-mobile/src/services/index.ts
+++ front-mobile/src/services/index.ts
@@ -1,8 +1,8 @@
 import AsyncStorage from '@react-native-community/async-storage';
 import axios from 'axios';
 
-//const BASE_URL = process.env.BASE_URL ?? 'http://localhost:8080';
-const BASE_URL = "http://192.168.3.27:8080";
+const BASE_URL = process.env.BASE_URL ?? 'https://insannity-movieflix.herokuapp.com';
+//const BASE_URL = "http://192.168.3.27:8080";
 
 export const TOKEN =  "Basic bW92aWVmbGl4Om1vdmllZmxpeDEyMw==";
 
